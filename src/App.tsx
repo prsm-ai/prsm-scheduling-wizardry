f76e2b4f2c1e6e8f4d9ac30189fec7fd3c883dc9
Mike <mikeschlosser16@gmail.com>
2025-07-18 16:33:24 -0500

    plz

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/prsm-scheduling-wizardry">
         <Navigation />
         <Routes>
           <Route path="/" element={<Index />} />
